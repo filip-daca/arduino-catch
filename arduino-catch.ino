5f2cebc6424cb7154bfdd710f26f10ba1cf429eb
filip-daca <filip.hubert.daca@gmail.com>
2020-12-25 18:51:35 +0100

    Custom pallet sprite

--- arduino-catch.ino
+++ arduino-catch.ino
@@ -1,18 +1,35 @@
 #include <LiquidCrystal.h>
+
 #define TOP_ROW 0
 #define BOTTOM_ROW 1
 
+#define PALLET_SPRITE 0
+
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); 
+
+byte palletSprite[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111
+};
+
+double readValue;
+int position;
 
 void setup() {
   lcd.begin(16, 2);
+
+  lcd.createChar(PALLET_SPRITE, palletSprite);
+  
   lcd.clear();
   lcd.setCursor(0, TOP_ROW); 
   lcd.print("Hej :)");
 }
-
-double readValue;
-int position;
 
 void loop() {
   position = 15 - analogRead(A0) * 15.0/1024.0;
@@ -30,5 +47,5 @@ void clearBottomRow() {
 
 void drawPallet() {
   lcd.setCursor(position, BOTTOM_ROW);
-  lcd.print("_");
+  lcd.write(byte(PALLET_SPRITE));
 }
